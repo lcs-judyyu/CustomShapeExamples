7eba0931dbcb5a6a1c6e0c9217f7461f1524b4be
Judy Yu <judy.yuqiuyi@gmail.com>
2022-11-17 11:41:31 -0500

    Initial Commit

---
+++ CustomShapeExamples/CustomShapeExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapeExamplesApp.swift
+//  CustomShapeExamples
+//
+//  Created by Judy YU on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapeExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
